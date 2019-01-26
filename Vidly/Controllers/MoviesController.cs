476d3674454f46085ad2b4198e2c4ba2a96707ec
Jose <jose8055@gmail.com>
2019-01-26 16:51:45 -0500

    Display the list of customers and movies

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -11,6 +11,23 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
         // GET: Movies
         public ActionResult Random()
         {
@@ -35,21 +52,15 @@ namespace Vidly.Controllers
             return Content("id=" + id);
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
+        
 
-            if(String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(string.Format("pageInde={0}&sortBy={1}", pageIndex, sortBy));
-        }
-
-        [Route("movies/released/{year}/{month}")]
+     /*   [Route("movies/released/{year}/{month}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year+"/"+month);
         }
+        
+     */
+
     }
 }
