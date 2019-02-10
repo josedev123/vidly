24cd0102b92f0defc24d3a34419d4cab099eaaff
Jose <jose8055@gmail.com>
2019-02-10 08:56:36 -0500

    Display customer's birthdate on their Prifile Page

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -21,14 +22,14 @@ namespace Vidly.Controllers
 
         public ViewResult Index()
         {
-            var customer = _context.Customers;
+            var customer = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customer);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
